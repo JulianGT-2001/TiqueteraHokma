8d4e559dce9c728baf95ecaf02300ac203f43e99
JULIAN DARIO GONZÁLEZ TOLEDO <julian.gonzalez@hokma.ai>
2022-10-13 13:55:59 -0500

    Se actualiza el modelo del proyecto.

--- Tiquetera/Models/TipoContactoViewModel.cs
+++ Tiquetera/Models/TipoContactoViewModel.cs
@@ -1,14 +1,14 @@
 ﻿namespace Tiquetera.Models
 {
-    public class ContactoViewModel
+    public class TipoContactoViewModel
     {
-        public int idContacto { get; set; }
         public int idTipoContacto { get; set; }
-        public string dato { get; set; }
+        public int padreId { get; set; }
+        public string descripcion { get; set; }
         public DateTime fechaCreacion { get; set; }
         public DateTime fechaModificacion { get; set; }
         public DateTime fechaMaquinaModificacion { get; set; }
-        public string maquinaModifica { get; set; }
+        public string maquinaModificacion { get; set; }
         public string maquinaCreacion { get; set; }
         public string usuarioCrea { get; set; }
         public string usuarioModifica { get; set; }
