cb44908d7929dccb7a5268b8c2da56f97c65fe72
JesusGomez <Jesus251964@hotmail.com>
2022-10-13 12:20:44 -0500

    Creacion de clase para conexion a base de datos

---
+++ Tiquetera/Datos/AplicationBaseDatos.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tiquetera.Datos
+{
+    public class AplicationBaseDatos : IdentityDbContext
+    {
+        public AplicationBaseDatos(DbContextOptions options): base(options)
+        {
+
+        }
+    }    
+}
