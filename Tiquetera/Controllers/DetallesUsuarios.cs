4189779a17a75d2e2c9d12a648ad5223e0fe14dd
JesusGomez <Jesus251964@hotmail.com>
2022-10-13 11:55:29 -0500

    Inicio de creacion de controladores CrearUsuarios, DetallesUsuarios y ListaUsuarios

---
+++ Tiquetera/Controllers/DetallesUsuarios.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tiquetera.Controllers
+{
+    public class DetallesUsuarios : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
