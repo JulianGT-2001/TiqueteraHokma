c251f146451eeb9f1a1cbcd0ff27e9b7780d1a83
JULIAN DARIO GONZÁLEZ TOLEDO <julian.gonzalez@hokma.ai>
2022-10-13 12:12:23 -0500

    Creo el controlador de listar sucursales.

---
+++ Tiquetera/Controllers/Sucursales.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tiquetera.Controllers
+{
+    public class Sucursales : Controller
+    {
+        public IActionResult ListarSucursales()
+        {
+            return View();
+        }
+    }
+}
